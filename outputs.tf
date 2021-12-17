5ddd4a4becd5e7f1a730359cdaa4edcec9465889
sgbyeon <sg.byeon1@gmail.com>
2021-12-17 15:05:32 +0900

    [dev] modified outputs.tf and variables.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
+output "account_id" {
+  description = "AWS Account ID"
+  value = var.account_id
+}
+
+output "region" {
+  description = "AWS region"
+  value = var.region
+}
+
 output "subnet_list" {
   description = "RDS Aurora Subnet List"
   value = aws_db_subnet_group.this.subnet_ids
